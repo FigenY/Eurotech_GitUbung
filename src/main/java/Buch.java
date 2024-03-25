55a56fdf6103c4535552bf4daa3b31bd629e25dd
User <figrnyurdakul38@gmail.com>
2024-03-25 12:09:16 +0100

    das erste Kapitel wurde korrigiert

--- src/main/java/Buch.java
+++ src/main/java/Buch.java
@@ -1,6 +1,7 @@
 public class Buch {
     public static void main(String[] args) {
         System.out.println("Mehmet schrieb das erste Kapital");//Strg+k für commit
+
         System.out.println("Mehmet hat das erste Kapital korrigiert");
 
     }
